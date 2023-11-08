9dba59d4f1116354e08d037035441e7f87d50651
Bfestus <f.bigiriman@alustudent.com>
2023-11-08 06:45:05 +0000

    fix bug

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,28 +3,15 @@
 Contains the number_of_subscribers function
 """
 
-import resquests
+import requests
 
 
 def number_of_subscribers(subreddit):
-
-    ## endpoint url
-    end_pointurl = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    ## Set a custom User-Agent header to avoid Too Many Requests error
-
-    headers = {'User-Agent': 'Custom User-Agent'}
-
-    ## Make a GET request to the Reddit API
-
-    response = resquests.get(end_pointurl, headers=headers)
-
-    ## check the status codes 
-
-    if response.status_code  == 200:
-        ## parse json response
-        data = response.json()
-        return data['data']['subscribers']
-    else:
-        ## if invalid return zero
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by /u/aaorrico23)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
