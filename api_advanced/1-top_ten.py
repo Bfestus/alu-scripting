adc8f07ce70b82df7f114890dea18d9bc1228b98
Bfestus <f.bigiriman@alustudent.com>
2023-11-09 08:49:03 +0000

    tasks

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """
-prints the titles of the first 10 hot posts listed
+Contains the top_ten function
 """
 
 import requests
 
-def  top_ten(subreddit):
-    """
-    creating endpoint url
-    """
 
+def  top_ten(subreddit):
+    """prints the titles of the top ten hot posts for a given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=10"
-    """ 
-    user agent 
-    """
     headers = {"User-Agent":"Mozilla/5.0"}
-    """
-    make get request
-    """
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-
         for post in data['data']['children']:
             print(post['data']['title'])
     else:
